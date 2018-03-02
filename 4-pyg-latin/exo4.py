6fc088b65149fcdd3b4d956fa81f78d6fc9f23e2
FlowCAD <florian.cadoz@gmail.com>
2018-03-02 15:29:13 +0100

    Lowercase and string getter added

--- 4-pyg-latin/exo4.py
+++ 4-pyg-latin/exo4.py
@@ -3,5 +3,7 @@ print 'Welcome to the Pig Latin Translator!'
 original = raw_input("Enter a word: ") 
 if len(original) > 0 and original.isalpha():
   print original
+  word = original.lower()
+  first = word[0]
 else:
   print 'empty'
