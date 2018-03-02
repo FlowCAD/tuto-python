feb0b959bc7e7775d76edc1359630c42217e5049
FlowCAD <florian.cadoz@gmail.com>
2018-03-02 16:20:48 +0100

    Function with parameters

--- 5-functions/exo5.py
+++ 5-functions/exo5.py
@@ -1,5 +1,14 @@
+# Basic function
 def spam():
   """Prints the string "Eggs!" to the console"""
   print "Eggs!"
 
 spam()
+
+# Function with parameters
+def power(base, exponent):
+  """raise the first argument to the power of the second"""
+  result = base ** exponent
+  print "%d to the power of %d is %d." % (base, exponent, result)
+
+power(37, 4)
