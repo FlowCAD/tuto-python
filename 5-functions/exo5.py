15250970b0d2cb4163c4c8960e8c68984f2de5ce
FlowCAD <florian.cadoz@gmail.com>
2018-03-02 17:08:15 +0100

    Function called by another one

--- 5-functions/exo5.py
+++ 5-functions/exo5.py
@@ -12,3 +12,20 @@ def power(base, exponent):
   print "%d to the power of %d is %d." % (base, exponent, result)
 
 power(37, 4)
+
+# Function called by another one
+def cube(number):
+  """return the cube of a number"""
+  return number ** 3
+
+def by_three(number):
+  """test if a number is divisible by 3"""
+  print number
+  if number%3 == 0:
+    print "Yes"
+    return cube(number)
+  else:
+    print "No"
+    return False
+
+by_three(9)
