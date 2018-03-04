00338aa2b8e1e90f29394b1c4a4477eaa9254f5e
FlowCAD <florian.cadoz@gmail.com>
2018-03-04 18:49:14 +0100

    List Slicing modified

--- 7-lists-and-dictionaries/exo7.py
+++ 7-lists-and-dictionaries/exo7.py
@@ -1,6 +1,6 @@
 # List Slicing
 suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
-first = suitcase[0:2]
+first = suitcase[:2]
 middle = suitcase[2:4]
-last = suitcase[4:6]
+last = suitcase[4:]
 print (first, middle, last)
