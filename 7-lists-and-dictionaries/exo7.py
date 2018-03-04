676fa9c5c23b81c674ceefb926841c2370c36e8c
FlowCAD <florian.cadoz@gmail.com>
2018-03-04 21:46:41 +0100

    Lists and Dictionaries

--- 7-lists-and-dictionaries/exo7.py
+++ 7-lists-and-dictionaries/exo7.py
@@ -18,3 +18,17 @@ for number in start_list:
 	square_list.append(number ** 2)
 square_list.sort()
 print square_list
+
+# Lists and Dictionaries
+inventory = {
+  'gold' : 500,
+  'pouch' : ['flint', 'twine', 'gemstone'],
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+inventory['pouch'].sort() 
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold'] += 50
+print inventory
