03d9bff67b4da9a3551a206149be04050f0d8d21
FlowCAD <florian.cadoz@gmail.com>
2018-03-04 19:00:07 +0100

    List Order

--- 7-lists-and-dictionaries/exo7.py
+++ 7-lists-and-dictionaries/exo7.py
@@ -4,3 +4,9 @@ first = suitcase[:2]
 middle = suitcase[2:4]
 last = suitcase[4:]
 print (first, middle, last)
+
+# Maintaining Order
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = animals.index("duck")
+animals.insert(duck_index, "cobra")
+print animals 
