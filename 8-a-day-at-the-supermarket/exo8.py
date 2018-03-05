57566d8ce3058a333fd14b2e33526a3dc58444a3
FlowCAD <florian.cadoz@gmail.com>
2018-03-05 20:00:40 +0100

    Calcul the price of a shopping list

--- 8-a-day-at-the-supermarket/exo8.py
+++ 8-a-day-at-the-supermarket/exo8.py
@@ -1,23 +1,39 @@
 # A Day at the Supermarket
 prices = {
-"banana": 4,
-"apple": 2,
-"orange": 1.5,
-"pear": 3
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
 }
 
 stock = {
-"banana": 6,
-"apple": 0,
-"orange": 32,
-"pear": 15
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
 }
 
-total = 0
-for key in prices:
-  print key
-  print "price: %s" % prices[key]
-  print "stock: %s" % stock[key]
-  total += prices[key] * stock[key]
 
-print total
+def calculate_stock_price(items):
+  """calculate the price of all the stock"""
+  total = 0
+  for key in items:
+    print key
+    print "price: %s" % prices[key]
+    print "stock: %s" % stock[key]
+    total += prices[key] * stock[key]
+  return total
+
+
+def compute_bill(food):
+  """calculate the price of my shopping list"""
+  total = 0
+  for item in food:
+    total += prices[item]
+  return total
+
+
+shopping_list = ["banana", "orange", "apple"]
+
+print calculate_stock_price(stock)
+print compute_bill(shopping_list)
