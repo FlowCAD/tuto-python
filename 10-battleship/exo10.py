dd73c4c552b3551c1f2e74b90b3d59f46db24d77
FlowCAD <florian.cadoz@gmail.com>
2018-03-08 17:46:44 +0100

    Hide a ship

--- 10-battleship/exo10.py
+++ 10-battleship/exo10.py
@@ -6,6 +6,13 @@ for i in range(5):
 
 def print_board(board_in):
   for row in board_in:
-    print row
+    print " ".join(row)
     
-print_board(board)
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
