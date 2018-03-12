00b4685fcefe0b23c9adcdac43ea41527cfc40e9
FlowCAD <florian.cadoz@gmail.com>
2018-03-12 16:39:06 +0100

    Masks

--- 12-bitwise-operators/exo12.py
+++ 12-bitwise-operators/exo12.py
@@ -22,3 +22,17 @@ print bin(shift_left)
 print "0b1110 and 0b101 : ", bin(0b1110 & 0b101)
 print "0b1110 or 0b101 : ", bin(0b1110 | 0b101)
 print "0b1110 xor 0b101 : ", bin(0b1110 ^ 0b101)
+
+
+# Masks
+def check_bit4(input):
+  """check if the fourth bit is on"""
+  mask = 0b1000
+  if (input & mask > 0):
+    return "on"
+  else:
+    return "off"
+
+"""Set the 3rd bit on"""
+a = 0b10111011
+print bin(a | 0b100)
