5591e6c35855c092d3810c33054d89df9b73a171
FlowCAD <florian.cadoz@gmail.com>
2018-03-12 15:45:06 +0100

    Bitwise Operations

---
+++ 12-bitwise-operators/exo12.py
@@ -0,0 +1,13 @@
+# Bitwise Operations
+print 0b1,    #1
+print 0b10,   #2
+print 0b11,   #3
+print 0b100,  #4
+print 0b101,  #5
+print 0b110,  #6
+print 0b111   #7
+print "******"
+print 0b1 + 0b11
+print 0b11 * 0b11
+print "******"
+print bin(4)
