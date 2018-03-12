c02adf551d3347cb78b7bb698761ff958cd9a7f2
FlowCAD <florian.cadoz@gmail.com>
2018-03-12 18:05:29 +0100

    Inheritance Syntax

--- 13-classes/exo13.py
+++ 13-classes/exo13.py
@@ -48,3 +48,15 @@ class ShoppingCart(object):
 
 my_cart = ShoppingCart("Flo")
 my_cart.add_item("Guitar", 100)
+
+
+# Inheritance Syntax
+class Shape(object):
+  def __init__(self, number_of_sides):
+    self.number_of_sides = number_of_sides
+
+class Triangle(Shape):
+  def __init__(self, side1, side2, side3):
+    self.side1 = side1
+    self.side2 = side2
+    self.side3 = side3
