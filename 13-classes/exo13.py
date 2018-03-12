e3d9367c6cd4853cc608da4e36a371b4d09ead8f
FlowCAD <florian.cadoz@gmail.com>
2018-03-12 17:23:59 +0100

    Basics

--- 13-classes/exo13.py
+++ 13-classes/exo13.py
@@ -1,8 +1,18 @@
 # POO !!
 
+# Class definition
 class Animal(object):
-  def __init__(self, name):
+  """Makes cute animals."""
+  # For initializing our instance objects
+  def __init__(self, name, age, is_hungry):
     self.name = name
-    
-zebra = Animal("Jeffrey")
-print zebra.name
+    self.age = age
+    self.is_hungry = is_hungry
+
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
