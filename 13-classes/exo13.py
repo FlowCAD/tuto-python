0c1acfc74631365d47f04ffc91fbf484eccea062
FlowCAD <florian.cadoz@gmail.com>
2018-03-12 17:44:06 +0100

    Class with method inside

--- 13-classes/exo13.py
+++ 13-classes/exo13.py
@@ -3,16 +3,21 @@
 # Class definition
 class Animal(object):
   """Makes cute animals."""
-  # For initializing our instance objects
+  is_alive = True
   def __init__(self, name, age, is_hungry):
     self.name = name
     self.age = age
     self.is_hungry = is_hungry
+  def description(self):
+    print self.name,
+    print self.age,
+    print self.is_hungry,
+    print self.is_alive
 
 zebra = Animal("Jeffrey", 2, True)
 giraffe = Animal("Bruce", 1, False)
 panda = Animal("Chad", 7, True)
 
-print zebra.name, zebra.age, zebra.is_hungry
-print giraffe.name, giraffe.age, giraffe.is_hungry
-print panda.name, panda.age, panda.is_hungry
+zebra.description()
+giraffe.description()
+panda.description()
