709d24d7a33960a2d274b627d8d7ed3acb880c55
FlowCAD <florian.cadoz@gmail.com>
2018-03-12 15:59:33 +0100

    Shifting

--- 12-bitwise-operators/exo12.py
+++ 12-bitwise-operators/exo12.py
@@ -4,10 +4,15 @@ print 0b10,   #2
 print 0b11,   #3
 print 0b100,  #4
 print 0b101,  #5
-print 0b110,  #6
-print 0b111   #7
-print "******"
-print 0b1 + 0b11
-print 0b11 * 0b11
-print "******"
+
+
+# Converting
 print bin(4)
+print int("0b100",2)
+
+
+# Shifting
+shift_right = 0b1100 >> 2
+shift_left = 0b1 << 2
+print bin(shift_right)
+print bin(shift_left)
