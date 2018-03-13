5e9f2192ef1b34fd96b1ed18efd42fbd6494891d
FlowCAD <florian.cadoz@gmail.com>
2018-03-13 13:23:25 +0100

    Reading the written data

--- 14-file-input-output/exo14.py
+++ 14-file-input-output/exo14.py
@@ -2,10 +2,13 @@
 
 #Writing the result of a list comprehension in an output file
 my_list = [i ** 2 for i in range(1, 11)]
-
 my_file = open("output.txt", "w")
-
 for i in my_list:
   my_file.write(str(i)+ "\n")
-  
+my_file.close()
+
+
+#Reading the written data
+my_file = open("output.txt", "r")
+print my_file.read()
 my_file.close()
